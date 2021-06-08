2196afcf56ecdfad77e980d9f24e2df726a50b20
Ives <ivescamiloto1@gmail.com>
2021-06-08 13:34:53 -0300

    Aula_04

---
+++ Auls/Aula_04.py
@@ -0,0 +1,22 @@
+# Laços de Repetição
+
+# Uso do FOR
+
+for x in range(10):
+    print(x)
+
+print('---------------------')
+
+# Comançando com um Numero X
+
+for x in range(1 , 10):
+    print(x)
+
+# Numeros primo com For Condicional
+print('---------------------')
+# Uso do While
+
+a = 0
+while a <= 10:
+    print(a)
+    a+=1
